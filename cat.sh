705a22b022e00710a2787b9691e0b609e668434c
psiblvdegod <artyombaly@list.ru>
2025-12-21 16:32:46 +0300

    add some aux stuff
    Signed-off-by: psiblvdegod <artyombaly@list.ru>

---
+++ cat.sh
@@ -0,0 +1,15 @@
+ls
+tree static
+tree templates
+echo -e "> cat templates/index.html\n"
+cat templates/index.html
+echo -e "> cat templates/features.html\n"
+cat cat templates/features.html
+echo -e "> cat static/css/cover.css\n"
+cat static/css/cover.css
+echo -e "> cat templates/contacts.html\n"
+cat cat templates/contacts.html
+echo -e "> cat templates/comments.html\n"
+cat cat templates/comments.html
+echo -e "> cat static/js/color-modes.js\n"
+cat static/js/color-modes.js
